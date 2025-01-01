9698105cec3d83d52453b6bb14af0d20076df3b9
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-01-02 00:16:23 +0100

    add concept to source step

--- open_icu/steps/source/base.py
+++ open_icu/steps/source/base.py
@@ -1,24 +1,31 @@
+from functools import lru_cache
+from importlib import import_module
 from pathlib import Path
-from typing import Iterator
+from typing import Any, Iterator
 
 import pandas as pd
 from pandera.typing import DataFrame
-from sqlalchemy import create_engine
+from sqlalchemy import Connection, create_engine
 
 from open_icu.steps.base import BaseStep
-from open_icu.types.base import SubjectData
+from open_icu.types.base import FHIRSchema, SubjectData
 from open_icu.types.conf.concept import Concept
 from open_icu.types.conf.source import SourceConfig
 
 
 class PandasDatabaseMixin:
-    def __init__(self, connection_uri: str) -> None:
-        engine = create_engine(connection_uri)
-        self._conn = engine.connect().execution_options(stream_results=True)
+    def __init__(self, connection_uri: str, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self._conn = self._create_conn(connection_uri)
 
-    def query_df(
+    @lru_cache
+    def _create_conn(self, connection_uri: str) -> Connection:
+        engine = create_engine(connection_uri)
+        return engine.connect().execution_options(stream_results=True)
+
+    def iter_query_df(
         self,
-        query: str = "",
+        sql: str = "",
         chunksize: int | None = None,
         **kwargs: str,
     ) -> Iterator[DataFrame]:
@@ -26,9 +33,22 @@ class PandasDatabaseMixin:
             self._conn as conn,
             conn.begin(),
         ):
-            for df in pd.read_sql_query(query.format(**kwargs), conn, chunksize=chunksize):
+            for df in pd.read_sql_query(sql.format(**kwargs), conn, chunksize=chunksize):
                 assert isinstance(df, pd.DataFrame)
                 yield df.pipe(DataFrame)
+
+    def get_query_df(
+        self,
+        sql: str = "",
+        **kwargs: str,
+    ) -> DataFrame:
+        with (
+            self._conn as conn,
+            conn.begin(),
+        ):
+            df = pd.read_sql_query(sql.format(**kwargs), conn, chunksize=None)
+            assert isinstance(df, pd.DataFrame)
+            return df.pipe(DataFrame)
 
 
 class Sampler(PandasDatabaseMixin):
@@ -37,33 +57,37 @@ class Sampler(PandasDatabaseMixin):
         FROM {table}
     """
 
-    def sample(self, source_config: SourceConfig) -> Iterator[SubjectData]:
-        for df in self.query_df(
-            query=self.QUERY,
+    def __init__(self, source_config: SourceConfig) -> None:
+        super().__init__(source_config.connection)
+        self._source_config = source_config
+
+    def sample(self) -> Iterator[SubjectData]:
+        for df in self.iter_query_df(
+            sql=self.QUERY,
             chunksize=1,
-            table=source_config.sample.table,
-            fields=source_config.sample.field,
+            table=self._source_config.sample.table,
+            fields=self._source_config.sample.field,
         ):
-            yield SubjectData(id=str(df.loc[0, source_config.sample.field]), source=source_config.name, data={})
+            yield SubjectData(
+                id=str(df.loc[0, self._source_config.sample.field]), source=self._source_config.name, data={}
+            )
 
 
 class SourceStep(BaseStep):
     def __init__(self, config_path: Path | None = None, parent: BaseStep | None = None) -> None:
         super().__init__(config_path, parent)
 
+        self._source_conigs: dict[str, SourceConfig] = {}
+        self._concepts = []
         if config_path is not None:
-            self._source_conigs = self._read_config(config_path / "sources", SourceConfig)
+            self._source_conigs = {conf.name: conf for conf in self._read_config(config_path / "sources", SourceConfig)}
             self._concepts = self._read_config(config_path / "concepts", Concept)
-        else:
-            self._source_conigs = []
-            self._concepts = []
 
     def __call__(self) -> Iterator[SubjectData]:
-        for source_config in self._source_conigs:
-            print(f"Sampling from {source_config.name}...")
-            sampler = Sampler(source_config.connection)
+        for source_config in self._source_conigs.values():
+            sampler = Sampler(source_config)
 
-            for subject_data in sampler.sample(source_config):
+            for subject_data in sampler.sample():
                 subject_data = self.pre_process(subject_data)
 
                 self.validate(subject_data)
@@ -76,4 +100,26 @@ class SourceStep(BaseStep):
                 yield subject_data
 
     def process(self, subject_data: SubjectData) -> SubjectData:
+        source = self._source_conigs[subject_data.source]
+        for concept in self._concepts:
+            for concept_source in concept.sources:
+                if concept_source.source != source.name:
+                    continue
+
+                module_name, cls_name = concept_source.extractor.rsplit(".", 1)
+                module = import_module(module_name)
+                cls = getattr(module, cls_name)
+                extractor = cls(source.connection, subject_data.id, concept, concept_source)
+
+                concept_data = extractor()
+                if concept_data is None:
+                    continue
+
+                if subject_data.data.get(concept.name, None) is None:
+                    subject_data.data[concept.name] = concept_data
+                else:
+                    subject_data.data[concept.name] = pd.concat([subject_data.data[concept.name], concept_data]).pipe(
+                        DataFrame[FHIRSchema]
+                    )
+
         return subject_data
