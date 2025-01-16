0c2d0343cc8cf646928c70fb7b3d497b03af7118
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-01-16 19:01:08 +0100

    add sink structure

--- open_icu/steps/sink/base.py
+++ open_icu/steps/sink/base.py
@@ -1,8 +1,7 @@
 from open_icu.steps.base import BaseStep
+from open_icu.types.base import SubjectData
 
 
 class SinkStep(BaseStep):
-    pass
-
-
-# todo: add sink steps
+    def process(self, subject_data: SubjectData) -> SubjectData:
+        return super().process(subject_data)
