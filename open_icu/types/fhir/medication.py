e451aed96ae901cafc4e0e82f58b6f7a51770fdd
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-01-17 21:07:55 +0100

    add flatten medication schema

--- open_icu/types/fhir/medication.py
+++ open_icu/types/fhir/medication.py
@@ -1,6 +1,10 @@
+from typing import Annotated
+
+import pandas as pd
 from pandera.typing import Series
 
 from open_icu.types.fhir import CodeableReference, Dosage, FHIRObjectSchema, Period, Reference
+from open_icu.types.fhir.base import FHIRFlattenSchema
 
 
 class FHIRObjectMedicationStatement(FHIRObjectSchema):
@@ -26,3 +30,12 @@ class FHIRObjectMedicationStatement(FHIRObjectSchema):
     effective_period: Series[Period]  # type: ignore[type-var]
     medication: Series[CodeableReference]  # type: ignore[type-var]
     dosage: Series[Dosage]  # type: ignore[type-var]
+
+
+class FHIRMedicationStatement(FHIRFlattenSchema):
+    effective_period__start: Series[Annotated[pd.DatetimeTZDtype, "ns", "utc"]]
+    effective_period__end: Series[Annotated[pd.DatetimeTZDtype, "ns", "utc"]]
+    dosage__dose_quantity__value: Series[float | int | str]
+    dosage__dose_quantity__unit: Series[str]
+    dosage__rate_quantity__value: Series[float | int | str]
+    dosage__rate_quantity__unit: Series[str]
