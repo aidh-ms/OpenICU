3886fe169b32e394793b2b025fc4db29c3d90986
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-01-30 20:16:12 +0100

    add doc strings for sink

--- open_icu/steps/sink/base.py
+++ open_icu/steps/sink/base.py
@@ -1,3 +1,4 @@
+from abc import ABCMeta
 from pathlib import Path
 
 from open_icu.steps.base import BaseStep
@@ -5,7 +6,22 @@ from open_icu.types.conf.concept import ConceptConfig
 from open_icu.types.conf.sink import SinkConfig
 
 
-class SinkStep(BaseStep[SinkConfig]):
+class SinkStep(BaseStep[SinkConfig], metaclass=ABCMeta):
+    """
+    A abstract class for sink steps.
+
+    Parameters
+    ----------
+    sink_path : Path
+        The path to the sink directory.
+    configs : Path | list[SinkConfig] | None
+        The path to the configuration files or a list of configurations.
+    concept_configs : Path | list[ConceptConfig] | None
+        The path to the concept configuration files or a list of configurations.
+    parent : BaseStep | None
+        The parent step.
+    """
+
     def __init__(
         self,
         sink_path: Path,
--- open_icu/steps/sink/csv.py
+++ open_icu/steps/sink/csv.py
@@ -3,7 +3,35 @@ from open_icu.types.base import SubjectData
 
 
 class CSVSinkStep(SinkStep):
+    """
+    A sink step that writes the data to a CSV files.
+
+    Parameters
+    ----------
+    sink_path : Path
+        The path to the sink directory.
+    configs : Path | list[SinkConfig] | None
+        The path to the configuration files or a list of configurations.
+    concept_configs : Path | list[ConceptConfig] | None
+        The path to the concept configuration files or a list of configurations.
+    parent : BaseStep | None
+        The parent step.
+    """
+
     def process(self, subject_data: SubjectData) -> SubjectData:
+        """
+        A method that writes the data to a CSV file per concept.
+
+        Parameters
+        ----------
+        subject_data : SubjectData
+            The subject data to write.
+
+        Returns
+        -------
+        SubjectData
+            The subject data that was written.
+        """
         if not self._sink_path.exists():
             self._sink_path.mkdir(parents=True)
 
