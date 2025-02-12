e3c7fc6faeabec706d0b25bfde658e17bf41c33a
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-02-12 19:39:38 +0100

    add doc string for db lib

--- open_icu/db/proto.py
+++ open_icu/db/proto.py
@@ -4,6 +4,10 @@ from pandera.typing import DataFrame
 
 
 class DataFrameDatabaseExtractorProto(Protocol):
+    """
+    A protocol for a database extractor class that returns pandas DataFrames.
+    """
+
     def __init__(self, conncetion_uri: str) -> None:
         ...
 
