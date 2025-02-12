100718460935df62df0f6832dc875ab78917c78e
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-02-12 19:32:25 +0100

    clean up

--- open_icu/steps/exceptions.py
+++
@@ -1,6 +0,0 @@
-class ValidationError(Exception):
-    """
-    Raised when a validation error occurs.
-
-    An exception that is raised from the validation of a step.
-    """
--- open_icu/steps/sink/__init__.py
+++
@@ -1,7 +0,0 @@
-from open_icu.steps.sink.csv import CSVSinkStep
-from open_icu.steps.sink.json import JSONSinkStep
-
-__all__ = [
-    "CSVSinkStep",
-    "JSONSinkStep",
-]
