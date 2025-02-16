13217500e4df7fa774bdc4e0fba0ad60d6f3af19
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-02-16 20:01:38 +0100

    add base concept

--- open_icu/step/concept/proto.py
+++ open_icu/step/concept/proto.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Iterator, Protocol
+from typing import TYPE_CHECKING, Any, Generic, Protocol, TypeVar
+
+from pandera.typing import DataFrame
 
 if TYPE_CHECKING:
     from open_icu.step.concept.conf import ConceptConfig, ConceptSourceConfig
-    from open_icu.type.subject import SubjectData
+    from open_icu.type.fhir import FHIRFlattenSchema
+
+FHIR_TYPE = TypeVar("FHIR_TYPE", bound=FHIRFlattenSchema)
 
 
-class IConceptService(Protocol):
+class IConceptService(Protocol, Generic[FHIR_TYPE]):
     """
     A protocol for the sampler service.
     """
@@ -15,5 +19,8 @@ class IConceptService(Protocol):
     def __init__(self, concept_source_config: ConceptSourceConfig, *args: Any, **kwargs: Any) -> None:
         ...
 
-    def __call__(self, concept_config: ConceptConfig, *args: Any, **kwargs: Any) -> Iterator[SubjectData]:
+    def __call__(self, concept_config: ConceptConfig, *args: Any, **kwargs: Any) -> DataFrame[FHIR_TYPE] | None:
+        ...
+
+    def _get_data(self, concept_source_config: ConceptSourceConfig, *args: Any, **kwargs: Any) -> DataFrame | None:
         ...
---
+++ open_icu/step/concept/services/concept/base.py
@@ -0,0 +1,127 @@
+from abc import ABCMeta, abstractmethod
+from typing import Any, Generic
+
+from pandera.typing import DataFrame
+
+from open_icu.step.concept.conf import ConceptConfig, ConceptSourceConfig
+from open_icu.step.concept.proto import FHIR_TYPE, IConceptService
+from open_icu.type.fhir.utils import to_identifiers_str
+
+
+class ConceptExtractor(IConceptService, Generic[FHIR_TYPE], metaclass=ABCMeta):
+    """
+    A base class for extracting data from a source based on a concept.
+
+    Parameters
+    ----------
+    subject_id : str
+        The subject ID to extract data for.
+    source : SourceConfig
+        The source configuration.
+    concept : ConceptConfig
+        The concept configuration.
+    concept_source : ConceptSource
+        The concept source configuration.
+    """
+
+    def __init__(self, concept_source_config: ConceptSourceConfig, *args: Any, **kwargs: Any) -> None:
+        self._concept_source_config = concept_source_config
+        self._args = args
+        self._kwargs = kwargs
+
+    @abstractmethod
+    def __call__(self, concept_config: ConceptConfig, *args: Any, **kwargs: Any) -> DataFrame[FHIR_TYPE] | None:
+        """
+        A shortcut to the `extract` method.
+
+        Parameters
+        ----------
+        concept_config : ConceptConfig
+            The concept configuration.
+        args : Any
+            The arguments to be passed to the extract method.
+        kwargs : Any
+            The keyword arguments to be passed to the extract method.
+
+        Returns
+        -------
+        DataFrame[F] | None
+            The extracted concept data.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def _get_data(self, concept_source_config: ConceptSourceConfig, *args: Any, **kwargs: Any) -> DataFrame | None:
+        """
+        A method to get the data from the source.
+
+        Parameters
+        ----------
+        concept_source_config : ConceptSourceConfig
+            The concept source configuration.
+        args : Any
+            The arguments to be passed to the extract method.
+        kwargs : Any
+            The keyword arguments to be passed to the extract method.
+
+        Returns
+        -------
+        DataFrame | None
+            The extracted data.
+        """
+        raise NotImplementedError
+
+    def _apply_identifier__coding(self, df: DataFrame, concept_config: ConceptConfig | None = None) -> str:
+        """
+        A method to apply to the concept DataFrame to add the identifier coding to the DataFrame.
+
+        Parameters
+        ----------
+        df : DataFrame
+            The DataFrame to apply the identifier coding to.
+        concept_config : ConceptConfig, optional
+            The concept configuration.
+
+        Returns
+        -------
+        str
+            The identifier coding.
+        """
+        assert concept_config is not None
+        return to_identifiers_str(concept_config.identifiers)
+
+    def _apply_subject__reference(self, df: DataFrame, concept_config: ConceptConfig | None = None) -> str:
+        """
+        A method to apply to the concept DataFrame to add the subject reference to the DataFrame.
+
+        Parameters
+        ----------
+        df : DataFrame
+            The DataFrame to apply the subject reference to.
+        concept_config : ConceptConfig, optional
+            The concept configuration.
+
+        Returns
+        -------
+        str
+            The subject reference.
+        """
+        return str(df["subject_id"])
+
+    def _apply_subject__type(self, df: DataFrame, concept_config: ConceptConfig | None = None) -> str:
+        """
+        A method to apply to the concept DataFrame to add the subject type to the DataFrame.
+
+        Parameters
+        ----------
+        df : DataFrame
+            The DataFrame to apply the subject type to.
+        concept_config : ConceptConfig, optional
+            The concept configuration.
+
+        Returns
+        -------
+        str
+            The subject type.
+        """
+        return self._concept_source_config.source
