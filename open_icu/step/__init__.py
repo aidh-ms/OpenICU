c110782bc4e348b3e1c7c0313997aa6974921039
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-02-23 12:21:38 +0000

    add filter step to import shortcuts

--- open_icu/step/__init__.py
+++ open_icu/step/__init__.py
@@ -1,9 +1,6 @@
+from open_icu.step.cohort.step import CohortFilterStep
 from open_icu.step.concept.step import ConceptStep
 from open_icu.step.source.step import SourceStep
 from open_icu.step.unit.step import UnitConversionStep
 
-__all__ = [
-    "SourceStep",
-    "ConceptStep",
-    "UnitConversionStep",
-]
+__all__ = ["SourceStep", "ConceptStep", "UnitConversionStep", "CohortFilterStep"]
