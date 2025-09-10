ca6247dda28749bea1e9aed21a1215b01dfe738d
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-09-10 13:31:25 +0000

    feat: update preprocessor

--- src/open_icu/meds/processor.py
+++ src/open_icu/meds/processor.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 import dask.dataframe as dd
-import pandas as pd
 
 from open_icu.config.source import TableConfig
+from open_icu.meds.schema import OpenICUMEDSData
 
 
 def process_table(table: TableConfig, path: Path, output_path: Path) -> None:
@@ -12,10 +12,10 @@ def process_table(table: TableConfig, path: Path, output_path: Path) -> None:
         for join_table in table.join
     }
     table_paths[table.name] = table.path
-    table_field_dtypes = table.table_field_dtypes()
+    table_field_dtypes = table.table_field_dtypes
 
     tables: dict[str, dd.DataFrame] = {}
-    for table_name, fields in table.table_field_names().items():
+    for table_name, fields in table.table_field_names.items():
         dtypes = {
             name: dtype if dtype != "datetime" else "string"
             for name, dtype in table_field_dtypes[table_name].items()
@@ -31,7 +31,7 @@ def process_table(table: TableConfig, path: Path, output_path: Path) -> None:
             dtype_backend="pyarrow",
         )
 
-        for const_field, const_value in table.table_constants().get(table_name, {}).items():
+        for const_field, const_value in table.table_constants.get(table_name, {}).items():
             ddf[const_field] = const_value
 
         tables[table_name] = ddf
@@ -40,33 +40,27 @@ def process_table(table: TableConfig, path: Path, output_path: Path) -> None:
     for join in table.join:
         df = df.merge(
             tables[join.name],
-            **join.join_params(),
+            **join.join_params,
             how=join.how
         )
 
     for event in table.events:
-        _df = df[event.field_names()]
+        _df = df[event.field_names]
         if event.fields.text_value is None:
             _df["text_value"] = ""
         if event.fields.numeric_value is None:
-            _df["numeric_value"] = 0.0
+            _df["numeric_value"] = 0
 
-        _df = _df.rename(columns={
-            event.fields.subject_id: "subject_id",
-            event.fields.time: "time",
-            event.fields.numeric_value: "numeric_value",
-            event.fields.text_value: "text_value",
-        })
+        _df = _df.dropna(subset=event.filters.dropna)
+        _df = _df.rename(columns=event.column_mapping)
 
-        _df = _df.dropna()
+        # def _apply(df: pd.DataFrame) -> pd.Series:
+        #     return df[event.fields.code[0]].str.cat(df[event.fields.code[1:]], sep="//")
 
-        def _apply(df: pd.DataFrame) -> pd.Series:
-            return df[event.fields.code[0]].str.cat(df[event.fields.code[1:]], sep="//")
-
-        codes = _df[event.fields.code].drop_duplicates()
+        codes = _df[event.fields.code].drop_duplicates().compute()
         # codes["code"] = codes[event.fields.code].apply(lambda c: c.str.cat(sep="//"), axis=1, meta=(None, "string"))
-        # codes["code"] = codes[event.fields.code[0]].str.cat(codes[event.fields.code[1:]], sep="//")
-        codes["code"] = codes.map_partitions(_apply, meta=(None, "string"))
+        codes["code"] = codes[event.fields.code[0]].str.cat(codes[event.fields.code[1:]], sep="//")
+        # codes["code"] = codes.map_partitions(_apply, meta=(None, "string"))
 
         _df = _df.merge(
             codes,
@@ -75,13 +69,15 @@ def process_table(table: TableConfig, path: Path, output_path: Path) -> None:
         )
 
         _df = _df.drop(columns=event.fields.code)
+
         _df.to_parquet(
             output_path / "data",
             name_function=lambda i: f"{table.name}_{event.name}_{i}.parquet",
-            write_index=False
+            write_index=False,
+            schema=OpenICUMEDSData.schema()
         )
 
-        _medadata_df = codes[["code"]]
+        _medadata_df = codes[["code"]].copy()
         _medadata_df["description"] = None
         _medadata_df["parent_codes"] = None
 
