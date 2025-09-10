04fd734542accc0f481e80df3baa4843b80030ce
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-09-10 13:26:59 +0000

    feat_ new config schema

--- src/open_icu/config/source.py
+++ src/open_icu/config/source.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field, computed_field
 
 from open_icu.config.base import Config
 
@@ -9,12 +9,27 @@ class MEDSFieldsConfig(BaseModel):
     code: list[str] = Field(default_factory=list)
     numeric_value: str | None = None
     text_value: str | None = None
+    extension: dict[str, str] = Field(default_factory=dict)
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def ext_field_names(self) -> list[str]:
+        return list(self.extension.values())
+
+
+class FilterConfig(BaseModel):
+    dropna: list[str] = Field(default_factory=list)
 
 
 class EventConfig(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     name: str
     fields: MEDSFieldsConfig
+    filters: FilterConfig = Field(default_factory=FilterConfig)
 
+    @computed_field  # type: ignore[prop-decorator]
+    @property
     def field_names(self) -> list[str]:
         return [*filter(None, [
             self.fields.subject_id,
@@ -22,7 +37,36 @@ class EventConfig(BaseModel):
             *self.fields.code,
             self.fields.numeric_value,
             self.fields.text_value,
+            *self.fields.ext_field_names
         ])]
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def column_mapping(self) -> dict[str, str]:
+        mappings = {}
+        if self.fields.subject_id:
+            mappings[self.fields.subject_id] = "subject_id"
+        if self.fields.time:
+            mappings[self.fields.time] = "time"
+        if self.fields.numeric_value:
+            mappings[self.fields.numeric_value] = "numeric_value"
+        if self.fields.text_value:
+            mappings[self.fields.text_value] = "text_value"
+
+        mappings.update(self.fields.extension)
+        return mappings
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def column_order(self) -> list[str]:
+        return [
+            "subject_id",
+            "time",
+            "code",
+            "numeric_value",
+            "text_value",
+            *sorted(self.fields.ext_field_names)
+        ]
 
 
 class FieldConfig(BaseModel):
@@ -40,6 +84,8 @@ class JoinConfig(BaseModel):
     right_on: list[str] = Field(default_factory=list)
     how: str = "left"
 
+    @computed_field  # type: ignore[prop-decorator]
+    @property
     def join_params(self) -> dict[str, list[str]]:
         params = {}
         if self.both_on:
@@ -59,6 +105,8 @@ class TableConfig(BaseModel):
     join: list[JoinConfig] = Field(default_factory=list)
     events: list[EventConfig] = Field(default_factory=list)
 
+    @computed_field  # type: ignore[prop-decorator]
+    @property
     def table_field_names(self) -> dict[str, list]:
         field_names = {}
         for join_table in self.join:
@@ -67,6 +115,8 @@ class TableConfig(BaseModel):
 
         return field_names
 
+    @computed_field  # type: ignore[prop-decorator]
+    @property
     def table_field_dtypes(self) -> dict[str, dict[str, str]]:
         field_dtypes = {}
         for join_table in self.join:
@@ -75,6 +125,8 @@ class TableConfig(BaseModel):
 
         return field_dtypes
 
+    @computed_field  # type: ignore[prop-decorator]
+    @property
     def table_constants(self) -> dict[str, dict[str, str | int | float]]:
         constants: dict[str, dict[str, str | int | float]] = {}
         for join_table in self.join:
