542717bfab29cf773821713c14297d7aa4e33566
Laser Tobias <q039tl@i-med.intern>
2025-11-13 10:08:25 +0100

    registered callback algebra.subtract

--- src/open_icu/transform/callbacks/__init__.py
+++ src/open_icu/transform/callbacks/__init__.py
@@ -2,6 +2,7 @@ from open_icu.transform.callbacks.filter import DropNa
 from open_icu.transform.callbacks.proto import CallbackProtocol
 from open_icu.transform.callbacks.regestry import CallbackRegistry, register_callback_class
 from open_icu.transform.callbacks.time import AddOffset, ToDatetime
+from open_icu.transform.callbacks.algebra import Subtract
 
 __all__ = [
     "CallbackRegistry",
