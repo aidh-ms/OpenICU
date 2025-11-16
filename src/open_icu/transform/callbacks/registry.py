91ae0d1e195e0b6addac9a99d75426f606a1bcbb
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-11-16 12:39:02 +0000

    wip: update callback regestry

--- src/open_icu/transform/callbacks/registry.py
+++ src/open_icu/transform/callbacks/registry.py
@@ -4,6 +4,7 @@ import re
 from functools import wraps
 from typing import Any, Callable
 
+from open_icu.helper.registry import BaseRegistry
 from open_icu.transform.callbacks.proto import CallbackProtocol
 
 CAMEL_CASE_PATTERN = re.compile(r'(?<!^)(?=[A-Z])')
@@ -18,42 +19,5 @@ def register_callback_class(cls: type[CallbackProtocol]) -> Callable[..., Callba
     return wrapper
 
 
-class CallbackRegistry:
-    _instance: CallbackRegistry | None = None
-    _callbacks: dict[str, type[CallbackProtocol]]
-
-    def __new__(cls) -> CallbackRegistry:
-        """Create or return the singleton instance."""
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance._callbacks = {}
-        return cls._instance
-
-    def register(
-        self,
-        name: str,
-        callback: type[CallbackProtocol],
-        overwrite: bool = False,
-    ) -> None:
-        if not overwrite and name in self._callbacks:
-            return
-
-        self._callbacks[name] = callback
-
-    def unregister(self, name: str) -> None:
-        del self._callbacks[name]
-
-    def get(self, name: str) -> type[CallbackProtocol] | None:
-        return self._callbacks.get(name)
-
-    def all(self) -> dict[str, type[CallbackProtocol]]:
-        return self._callbacks.copy()
-
-    def clear(self) -> None:
-        self._callbacks.clear()
-
-    def __contains__(self, name: str) -> bool:
-        return name in self._callbacks
-
-    def __len__(self) -> int:
-        return len(self._callbacks)
+class CallbackRegistry(BaseRegistry[type[CallbackProtocol]]):
+    pass
