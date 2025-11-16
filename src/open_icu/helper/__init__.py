c72da81f976ef726faf60a1f93cdad70b9a3e246
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-11-16 12:38:04 +0000

    feat: add base regestry pattern

--- src/open_icu/helper/__init__.py
+++ src/open_icu/helper/__init__.py
@@ -1,6 +1,7 @@
 """Helper utilities for OpenICU."""
 
+from open_icu.helper.config import BaseConfig
 from open_icu.helper.registry import BaseRegistry
 from open_icu.helper.singelton import SingletonMeta
 
-__all__ = ["BaseRegistry", "SingletonMeta"]
+__all__ = ["BaseRegistry", "SingletonMeta", "BaseConfig"]
