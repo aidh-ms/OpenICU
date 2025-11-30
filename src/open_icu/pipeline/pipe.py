d08f9b5c2acb8752ce046ba811b71698fd6b2f75
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-11-30 11:35:37 +0000

    wip: add structure

--- src/open_icu/pipeline/pipe.py
+++ src/open_icu/pipeline/pipe.py
@@ -1,2 +1,15 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+
 class Pipeline:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def from_config_path(cls, path: str | Path) -> Pipeline:
+        return cls()
+
+    def run(self) -> None:
+        pass
