81dd23b281c374a8d0e3867db6f9f0255b1638bb
Tobias Verona Laser <55537948+TobiasVeronaLaser@users.noreply.github.com>
2025-12-29 16:13:06 +0000

    fix: ignore type

--- src/open_icu/transform/callbacks/algebra.py
+++ src/open_icu/transform/callbacks/algebra.py
@@ -306,9 +306,9 @@ class Root(HybridCallback):
             A Polars expression computing the signed n-th root.
         """
         return (
-            self.radicand.sign()
-            * self.radicand.abs() ** (1 / float(self.index))
-        )  # type: ignore
+            self.radicand.sign()                                # type: ignore
+            * self.radicand.abs() ** (1 / float(self.index))    # type: ignore
+        )
 
     def as_field(self, lf: LazyFrame) -> LazyFrame:
         """Apply the root operation and add the result as a new column.
