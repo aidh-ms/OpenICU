22267cbc5eeb3068db7ad89e4ecefd98795375ec
Tobias Verona Laser <55537948+TobiasVeronaLaser@users.noreply.github.com>
2025-12-29 09:22:25 +0000

    revert "update from main manually"

--- src/open_icu/transform/callbacks/__init__.py
+++ src/open_icu/transform/callbacks/__init__.py
@@ -2,7 +2,6 @@ from open_icu.transform.callbacks.filter import DropNa
 from open_icu.transform.callbacks.proto import CallbackProtocol
 from open_icu.transform.callbacks.registry import CallbackRegistry, register_callback_class
 from open_icu.transform.callbacks.time import AddOffset, ToDatetime
-from open_icu.transform.callbacks.selector import FirstNotNull
 from open_icu.transform.callbacks.algebra_v2 import Add, Sum, Subtract, Multiply, Product, Divide, Pow, Root, Modulo
 from open_icu.transform.callbacks.ast_expression_evaluator_v2 import AbstractSyntaxTree
 
@@ -14,8 +13,6 @@ __all__ = [
     "DropNa",
     "ToDatetime",
     "AddOffset",
-
-    "FirstNotNull",
 
     "Add",
     "Sum",
