afa031f9a9b61ce1f27580b7b6f13b50f09f18dc
Tobias Verona Laser <55537948+TobiasVeronaLaser@users.noreply.github.com>
2025-12-29 16:09:04 +0000

    fix: import

--- src/open_icu/transform/callbacks/__init__.py
+++ src/open_icu/transform/callbacks/__init__.py
@@ -2,8 +2,8 @@ from open_icu.transform.callbacks.filter import DropNa
 from open_icu.transform.callbacks.proto import CallbackProtocol
 from open_icu.transform.callbacks.registry import CallbackRegistry, register_callback_class
 from open_icu.transform.callbacks.time import AddOffset, ToDatetime
-from open_icu.transform.callbacks.algebra_v2 import Add, Sum, Subtract, Multiply, Product, Divide, Pow, Root, Modulo
-from open_icu.transform.callbacks.ast_expression_evaluator_v2 import AbstractSyntaxTree
+from open_icu.transform.callbacks.algebra import Add, Sum, Subtract, Multiply, Product, Divide, Pow, Root, Modulo
+from open_icu.transform.callbacks.ast_expression_evaluator import AbstractSyntaxTree
 
 __all__ = [
     "CallbackRegistry",
