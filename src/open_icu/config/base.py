0230a15015764ee3705aabcafa33db9c75892677
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2026-01-01 22:32:37 +0000

    feat: add base config

---
+++ src/open_icu/config/base.py
@@ -0,0 +1,40 @@
+from abc import ABCMeta
+from pathlib import Path
+from uuid import UUID, uuid5
+
+import yaml
+from pydantic import BaseModel, computed_field
+
+
+class BaseConfig(BaseModel, metaclass=ABCMeta):
+    name: str
+    version: str
+
+    @property
+    @computed_field
+    def identifier(self) -> str:
+        """Generate an OID based on the name and version of the config."""
+        return self.build_identifier(self.name, self.version)
+
+    @property
+    @computed_field
+    def uuid(self) -> UUID:
+        """Generate a UUID based on the name and version of the config."""
+        return uuid5(uuid5.NAMESPACE_DNS, self.identifier)  # type: ignore[unresolved-attribute]
+
+    @classmethod
+    def build_identifier(cls, name: str, version: str) -> str:
+        return f"openicu.config.{cls.__name__.lower()}.{name.lower()}.{version.lower()}"
+
+    @classmethod
+    def load(cls, file_path: Path) -> "BaseConfig":
+        """Load configuration from a dictionary."""
+        with open(file_path, "r") as f:
+            data = yaml.safe_load(f)
+
+        return cls(**data)
+
+    def save(self, file_path: Path) -> None:
+        """Save configuration to a YAML file."""
+        with open(file_path, "w") as f:
+            yaml.safe_dump(self.model_dump(mode="json"), f)
