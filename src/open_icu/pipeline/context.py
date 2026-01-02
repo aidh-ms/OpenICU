e37d73a1b3a51e3953a8d735d2182641271db8b5
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2026-01-02 16:08:12 +0000

    feat: add pipeline context

---
+++ src/open_icu/pipeline/context.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+from open_icu.storage.project import OpenICUProject
+
+
+@dataclass
+class PipelineContext:
+    project: OpenICUProject
+
+
+
+
+
+
+
+# create a context for pipeline execution
+# has open icu project object
+# create a state model for pipeline execution
