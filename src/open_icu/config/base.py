5ecaa43eab0e03e1f6d9cfbf7566bc864142bcb8
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2026-01-03 23:35:35 +0000

    wip: update base config

--- src/open_icu/config/base.py
+++ src/open_icu/config/base.py
@@ -15,7 +15,12 @@ class BaseConfig(BaseModel, metaclass=ABCMeta):
     @property
     def identifier(self) -> str:
         """Generate an OID based on the name and version of the config."""
-        return self.build_identifier(self.name, self.version)
+        return self.build_identifier(self.identifier_tuple)
+
+    @computed_field
+    @property
+    def identifier_tuple(self) -> tuple[str, ...]:
+        return self.__class__.__name__, self.version, self.name
 
     @computed_field
     @property
@@ -24,8 +29,9 @@ class BaseConfig(BaseModel, metaclass=ABCMeta):
         return uuid5(NAMESPACE_DNS, self.identifier)  # type: ignore[unresolved-attribute]
 
     @classmethod
-    def build_identifier(cls, name: str, version: str) -> str:
-        return f"openicu.config.{cls.__name__.lower()}.{name.lower()}.{version.lower()}"
+    def build_identifier(cls, t: tuple[str, ...]) -> str:
+        id = ".".join(t).lower()
+        return f"openicu.config.{id}"
 
     @classmethod
     def load(cls, file_path: Path) -> Self:
@@ -35,7 +41,11 @@ class BaseConfig(BaseModel, metaclass=ABCMeta):
 
         return cls(**data)
 
-    def save(self, file_path: Path) -> None:
+    def save(self, path: Path) -> None:
         """Save configuration to a YAML file."""
-        with open(file_path, "w") as f:
-            yaml.safe_dump(self.model_dump(mode="json"), f)
+
+        path = Path(path, *self.identifier_tuple).with_suffix(".yml")
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+        with open(path, "w") as f:
+            yaml.safe_dump(self.model_dump(mode="json", exclude_computed_fields=True), f)
