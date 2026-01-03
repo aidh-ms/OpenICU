3a2d210a972307d169166491fc11ff851f636bc8
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2026-01-03 23:36:26 +0000

    wip: add handling of configs

--- src/open_icu/steps/base/step.py
+++ src/open_icu/steps/base/step.py
@@ -28,8 +28,18 @@ class BaseStep[T: BaseStepConfig](ABC):
 class ConfigurableBaseStep[PCT: ConfigurableBaseStepConfig, SCT: BaseConfig](BaseStep[PCT], metaclass=ABCMeta):
     def __init__(self, context: PipelineContext, config: dict[str, Any]) -> None:
         super().__init__(context, config)
+        self._load_subconfig()
 
-        self._config_registery = BaseConfigRegistery[SCT]()
+    @property
+    def _subconfig_type(self) -> type[SCT]:
+        t = get_generic_type(self.__class__, 1)
+        return cast(type[SCT], t)
+
+    def _load_subconfig(self) -> None:
+        class _Registry(BaseConfigRegistery[self._subconfig_type]):  # type: ignore[invalid-type-form]
+            pass
+
+        self._config_registery = _Registry()
 
         for sub_config in self._config.files:
             configs = load_config(sub_config.path, self._subconfig_type)
@@ -38,7 +48,4 @@ class ConfigurableBaseStep[PCT: ConfigurableBaseStepConfig, SCT: BaseConfig](Bas
             for cfg in filtered_configs:
                 self._config_registery.register(cfg, overwrite=sub_config.overwrite)
 
-    @property
-    def _subconfig_type(self) -> type[SCT]:
-        t = get_generic_type(self.__class__, 1)
-        return cast(type[SCT], t)
+        self._config_registery.save(self._context.project.configs_path)
