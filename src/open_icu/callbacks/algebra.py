8aaab4b7812a78e9735e2378bed472d37aafbf38
Tobias Verona Laser <55537948+TobiasVeronaLaser@users.noreply.github.com>
2026-01-17 10:50:56 +0000

    feat: nested Sum Callback

--- src/open_icu/callbacks/algebra.py
+++ src/open_icu/callbacks/algebra.py
@@ -20,102 +20,102 @@ class Add(CallbackProtocol):
         expr = to_expr(lf, self.augend) + to_expr(lf, self.addend)
 
         if self.output is None:
-            # Expression mode (nesting)
             return expr
 
-        # Frame mode (write column)
         return lf.with_columns(expr.alias(self.output))
 
 @register_callback_class
 class Sum(CallbackProtocol):
-    def __init__(self, summands: list[str], output: str) -> None:
+    def __init__(self, summands: list[str], output: Optional[str] = None) -> None:
         self.summands = summands
         self.output = output
 
-    def __call__(self, lf: LazyFrame) -> LazyFrame:
-        return lf.with_columns(
-            (pl.sum_horizontal([pl.col(c) for c in self.summands]).alias(self.output))
-        )
+    def __call__(self, lf: LazyFrame) -> CallbackResult:
+        expr = to_expr(lf, pl.sum_horizontal([pl.col(c) for c in self.summands]))
+        if self.output is None:
+            return expr
+        
+        return lf.with_columns(expr.alias(self.output))
 
 @register_callback_class
 class Subtract(CallbackProtocol):
-    def __init__(self, minuend: str, subtrahend: str, output: str) -> None:
+    def __init__(self, minuend: str, subtrahend: str, output: Optional[str] = None) -> None:
         self.minuend = minuend
         self.subtrahend = subtrahend
         self.output = output
 
-    def __call__(self, lf: LazyFrame) -> LazyFrame:
+    def __call__(self, lf: LazyFrame) -> CallbackResult:
         return lf.with_columns(
             (pl.col(self.minuend) - pl.col(self.subtrahend)).alias(self.output)
         )
 
 @register_callback_class
 class Multiply(CallbackProtocol):
-    def __init__(self, multiplicand: str, multiplier: str, output: str) -> None:
+    def __init__(self, multiplicand: str, multiplier: str, output: Optional[str] = None) -> None:
         self.multiplicand = multiplicand
         self.multiplier = multiplier
         self.output = output
 
-    def __call__(self, lf: LazyFrame) -> LazyFrame:
+    def __call__(self, lf: LazyFrame) -> CallbackResult:
         return lf.with_columns(
             (pl.col(self.multiplicand) * pl.col(self.multiplier)).alias(self.output)
         )
 
 @register_callback_class
 class Product(CallbackProtocol):
-    def __init__(self, factor: list[str], output: str) -> None:
+    def __init__(self, factor: list[str], output: Optional[str] = None) -> None:
         self.factor = factor
         self.output = output
 
-    def __call__(self, lf: LazyFrame) -> LazyFrame:
+    def __call__(self, lf: LazyFrame) -> CallbackResult:
         return lf.with_columns(
             (pl.fold(acc=pl.lit(1),function=operator.mul, exprs=pl.col(self.factor)).alias(self.output))
         )
 
 @register_callback_class
 class Divide(CallbackProtocol):
-    def __init__(self, dividend: str, divisor: str, output: str) -> None:
+    def __init__(self, dividend: str, divisor: str, output: Optional[str] = None) -> None:
         self.dividend = dividend
         self.divisor = divisor
         self.output = output
 
-    def __call__(self, lf: LazyFrame) -> LazyFrame:
+    def __call__(self, lf: LazyFrame) -> CallbackResult:
         return lf.with_columns(
             (pl.col(self.dividend) / pl.col(self.divisor)).alias(self.output)
         )
 
 @register_callback_class
 class Pow(CallbackProtocol):
-    def __init__(self, base: str, exponent: str, output: str) -> None:
+    def __init__(self, base: str, exponent: str, output: Optional[str] = None) -> None:
         self.base = base
         self.exponent = exponent
         self.output = output
 
-    def __call__(self, lf: LazyFrame) -> LazyFrame:
+    def __call__(self, lf: LazyFrame) -> CallbackResult:
         return lf.with_columns(
             (pl.col(self.base) ** pl.col(self.exponent)).alias(self.output)
         )
 
 @register_callback_class
 class Root(CallbackProtocol):
-    def __init__(self, radicand: str, index: Real, output: str) -> None:
+    def __init__(self, radicand: str, index: Real, output: Optional[str] = None) -> None:
         self.radicand = radicand
         self.index = index
         self.output = output
 
-    def __call__(self, lf: LazyFrame) -> LazyFrame:
+    def __call__(self, lf: LazyFrame) -> CallbackResult:
         return lf.with_columns(
             (pl.col(self.radicand).sign() * (pl.col(self.radicand).abs() ** (1 / float(self.index))).alias(self.output))
         )
 
 @register_callback_class
 class Modulo(CallbackProtocol):
-    def __init__(self, dividend: str, divisor: str, output: str) -> None:
+    def __init__(self, dividend: str, divisor: str, output: Optional[str] = None) -> None:
         self.dividend = dividend
         self.divisor = divisor
         self.output = output
 
-    def __call__(self, lf: LazyFrame) -> LazyFrame:
+    def __call__(self, lf: LazyFrame) -> CallbackResult:
         return lf.with_columns(
             (pl.col(self.dividend) % pl.col(self.divisor)).alias(self.output)
         )
