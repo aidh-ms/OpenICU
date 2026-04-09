427d14f0cdbdd82af4a4119c3aed6bc78349ba48
TobiasVeronaLaser <tobias.laser.ni@gmail.com>
2026-04-09 11:03:17 +0000

    feat: init sharding step

---
+++ src/open_icu/steps/sharding/config/sharding.py
@@ -0,0 +1,30 @@
+from pydantic import Field
+
+from open_icu.config.base import BaseConfig
+
+
+class ShardingConfig(BaseConfig):
+    """Configuration for a sharding table.
+
+    Attributes:
+        TODO:
+        ...
+    """
+
+    __open_icu_config_type__ = "sharding"
+
+    concepts: list[str] = Field()       # all-include, include, exclude
+
+    # Nächste
+
+    subject_ids: list[str] = Field()    # all-includes, include, exclude
+
+    # ERstmal ignorieren
+    time_spacing: str = Field()         # TODO: naming
+    presplit: bool = Field()            # true, false (split before sharding)
+    grouping_size = Field()        # patients per parquet
+
+    """ """
+
+
+# multithread auf parquet ein thread paar subjects
